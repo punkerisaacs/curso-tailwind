3aa53717c5249094b1342b7568b7740e6f5d8f64
punkerisaacs <punkerisaacs@hotmail.com>
2022-08-27 21:30:31 -0500

    end

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
             screens: {
               'xs': {max: '768px'}
             },
+            boxShadow: {
+              headers3D: "0px 1px 0px #393D3F, 1px 2px #393D3F, 2px 3px 0px #393D3F, 3px 4px 0px #393D3F",
+              box: "0px 0px 1px rgba(0,0,0, 0.3), 0px 3px 7px rgba(0,0,0, 0.3), 0px 1px white inset, 0px -3px rgba(0,0,0, 0.3) inset"
+            },
             backgroundImage: theme => ({
                 'body-pattern': "url('../img/pattern.png')",
                 banner: "url('../img/bakbaner.png')"
@@ -19,7 +23,10 @@ module.exports = {
             animation: {
                 "spin-slow": "spin 2s linear infinite",
                 'bg-banner': 'backBanner 10s linear',
-                'text-banner': 'textBanner 10s linear'
+                'text-banner': 'textBanner 10s linear',
+                "show-card-icon": 'showCardIcon 300ms linear',
+                "show-card-category": 'showCardCategory 400ms linear',
+                "show-card-description": 'showCardDescription 400ms linear'
             },
             keyframes: {
                 backBanner: {
@@ -31,7 +38,19 @@ module.exports = {
                     '50%': { transform: 'translate(600%) scale(5,5)', opacity: 1},
                     '75%': { transform: 'translate(-600%) scale(5,5)', textShadow: 'none', color: 'transparent', opacity: 0},
                     '100%':{ transform: 'translate(0%) scale(1,1)', textShadow: '2px 2px 1px black', color: 'white', opacity: 1},
-                }
+                },
+                showCardIcon: {
+                    'from': { transform: 'translateY(-200%)'},
+                    'to':{ transform: 'translateY(0%)'},
+                },
+                showCardCategory: {
+                    'from': { transform: 'translateY(-300%)'},
+                    'to':{ transform: 'translateY(0%)'},
+                },
+                showCardDescription: {
+                    'from': { transform: 'translateX(-300%)'},
+                    'to':{ transform: 'translateX(0%)'},
+                },
             }
         },
     },
@@ -39,6 +58,10 @@ module.exports = {
         animation: ['responsive', 'hover', 'group-hover'],
         animate: ['responsive', 'hover', 'group-hover'],
         fontSize: ['responsive', 'hover', 'group-hover'],
+        scale: ['responsive', 'hover', 'group-hover'],
+        padding: ['responsive', 'hover', 'group-hover'],
+        transform: ['responsive', 'hover', 'group-hover'],
+        translate: ['responsive', 'hover', 'group-hover'],
     },
     plugins: [],
 }
